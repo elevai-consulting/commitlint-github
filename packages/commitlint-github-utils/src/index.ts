0262ca67beadd0f692fb3e40cb9a9c8100f7f7ff
Dan Rivett <dan.rivett@elevai.com>
2020-02-15 07:20:30 -0800

    (#1) Initial changes to commitlint-github-utils.

--- packages/commitlint-github-utils/src/index.ts
+++ packages/commitlint-github-utils/src/index.ts
@@ -1,11 +1,11 @@
-import * as commitlintGitHubConstants from './commitlintGitHubConstants'
-import parseCommitMessage from './parseCommitMessage'
-import { CommitlintGitHubUtils } from '../@types'
+import * as commitlintGitHubConstants from './commitlintGitHubConstants';
+import parseCommitMessage from './parseCommitMessage';
+import { CommitlintGitHubUtils } from '../@types';
 
 const commitlintGitHubUtils: CommitlintGitHubUtils = {
   commitlintGitHubConstants,
   parseCommitMessage,
-}
+};
 
-export { commitlintGitHubConstants, parseCommitMessage }
-export default commitlintGitHubUtils
+export { commitlintGitHubConstants, parseCommitMessage };
+export default commitlintGitHubUtils;
--- packages/commitlint-github-utils/src/tests/parseCommitMessage.test.ts
+++ packages/commitlint-github-utils/src/tests/parseCommitMessage.test.ts
@@ -1,22 +1,10 @@
-import parseCommitMessage from '../parseCommitMessage'
+/* eslint-disable prettier/prettier */
+import parseCommitMessage from '../parseCommitMessage';
 
-const COMMIT_MESSAGE = 'Test commit message'
+const COMMIT_MESSAGE = 'Test commit message';
 
 describe('commitlintPluginGitHubTests', () => {
   const testCommitMessages = {
-    singleScope: `(#1) ${COMMIT_MESSAGE}`,
-    singleScopeMultipleDigits: `(#1234) ${COMMIT_MESSAGE}`,
-    multiScope: `(#1, #2) ${COMMIT_MESSAGE}`,
-    multiScopeMultipleDigits: `(#1, #34) ${COMMIT_MESSAGE}`,
-    singleScopeTypedTask: `(#123) chore: ${COMMIT_MESSAGE}`,
-    multiScopeTypedTask: `(#123, #45) chore: ${COMMIT_MESSAGE}`,
-    singleScopeWipTask: `(#123) WIP: ${COMMIT_MESSAGE}`,
-    multiScopeWipTask: `(#123, #45) WIP: ${COMMIT_MESSAGE}`,
-    issueNumberMissing: 'My commit message',
-    issueNumberEmpty: '() My commit message',
-    issueNumberMissingHash: '(123) My commit message',
-    issueNumberNonNumeric: '(#bob) My commit message',
-    multiCommitPartsSeparator: `(#42) ${COMMIT_MESSAGE} http://gherciu.github.io`,
     multiLineCommit: `
       (#123) ${COMMIT_MESSAGE}
 
@@ -24,55 +12,264 @@ describe('commitlintPluginGitHubTests', () => {
         - SUBTASK-1: I added a new feature
         * SUBTASK-2: I fixed a issue
     `,
-  }
+  };
 
-  it('should return correct commitTaskIds', () => {
+  it('should return correct issue numbers', () => {
     expect(
-      parseCommitMessage(testCommitMessages.singleScope).commitTaskIds,
-    ).toEqual(['IB-2121'])
-    // expect(
-    //   parseCommitMessage(testCommitMessages.singleScopeWipTask).commitTaskIds,
-    // ).toEqual(['IB-2121'])
-    expect(
-      parseCommitMessage(testCommitMessages.multiScope).commitTaskIds,
-    ).toEqual(['IB-2121', 'IB-21'])
-    // expect(
-    //   parseCommitMessage(testCommitMessages.multiScopeWipTask).commitTaskIds,
-    // ).toEqual(['IB-2121', 'IB-21'])
-  })
+      parseCommitMessage(`(#1) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([1]);
 
-  it('should return correct commitFooter', () => {
     expect(
-      parseCommitMessage(testCommitMessages.singleScope).commitFooter,
-    ).toEqual('test commit message')
-  })
+      parseCommitMessage(`(#1234) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([1234]);
 
-  it('should return empty array of taskIds', () => {
     expect(
-      parseCommitMessage(testCommitMessages.emptyTaskIds).commitTaskIds,
-    ).toEqual([])
-    expect(
-      parseCommitMessage(testCommitMessages.missingSeparator).commitTaskIds,
-    ).toEqual([])
-  })
+      parseCommitMessage(`(#1, #2) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([1, 2]);
 
-  it('should return corect taskIds and commit footer if a url is added in commit message', () => {
     expect(
-      parseCommitMessage(testCommitMessages.multiCommitPartsSeparator)
-        .commitTaskIds,
-    ).toEqual(['IB-2121'])
-    expect(
-      parseCommitMessage(testCommitMessages.multiCommitPartsSeparator)
-        .commitFooter,
-    ).toEqual('test commit message http://gherciu.github.io')
-  })
+      parseCommitMessage(`(#1,#2) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([1, 2]);
 
-  it('should return corect taskIds and commit footer if is provided a multiline commit message used for description', () => {
     expect(
-      parseCommitMessage(testCommitMessages.multiLineCommit).commitTaskIds,
-    ).toEqual(['IB-2121'])
+      parseCommitMessage(`(#1, #34) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([1, 34]);
+
     expect(
-      parseCommitMessage(testCommitMessages.multiLineCommit).commitFooter,
-    ).toEqual('test commit message')
-  })
-})
+      parseCommitMessage(`(#1,#34) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([1, 34]);
+
+    expect(
+      parseCommitMessage(`(#123) chore: ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([123]);
+
+    expect(
+      parseCommitMessage(`(#123, #45) chore: ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([123, 45]);
+
+    expect(
+      parseCommitMessage(`(#123,#45) chore: ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([123, 45]);
+
+    expect(
+      parseCommitMessage(`(#123) WIP: ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([123]);
+
+    expect(
+      parseCommitMessage(`(#123, #45) WIP: ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([123, 45]);
+
+    expect(
+      parseCommitMessage(`(#123,#45) WIP: ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([123, 45]);
+  });
+
+  it('should return no issue numbers', () => {
+    // Missing issue numbers prefix entirely
+    expect(
+      parseCommitMessage('My commit message').issueNumbers
+    ).toEqual([]);
+
+    // Empty issue numbers prefix
+    expect(
+      parseCommitMessage(`() ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    // Non-numeric issue numbers prefix
+    expect(
+      parseCommitMessage(`(#2bob) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#2, #1bob) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#2,#1bob) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#2, 1bob) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#2,1bob) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#bob, #2) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#1bob,#2) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#1bob, 2) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#1bob,2) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    // Missing issue number hash
+    expect(
+      parseCommitMessage(`(123) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#1, 123) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(#1,123) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(1, #123) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+
+    expect(
+      parseCommitMessage(`(1,#123) ${COMMIT_MESSAGE}`).issueNumbers
+    ).toEqual([]);
+  });
+
+  it('should return correct type', () => {
+    expect(
+      parseCommitMessage(`(#1) ${COMMIT_MESSAGE}`).type
+    ).toEqual(null);
+
+    expect(
+      parseCommitMessage(`(#1, #2) ${COMMIT_MESSAGE}`).type
+    ).toEqual(null);
+
+    expect(
+      parseCommitMessage(`(#1,#2) ${COMMIT_MESSAGE}`).type
+    ).toEqual(null);
+
+    expect(
+      parseCommitMessage(`(#123) chore: ${COMMIT_MESSAGE}`).type
+    ).toEqual('chore');
+
+    expect(
+      parseCommitMessage(`(#123, #45) chore: ${COMMIT_MESSAGE}`).type
+    ).toEqual('chore');
+
+    expect(
+      parseCommitMessage(`(#123,#45) chore: ${COMMIT_MESSAGE}`).type
+    ).toEqual('chore');
+
+    expect(
+      parseCommitMessage(`(#123) WIP: ${COMMIT_MESSAGE}`).type
+    ).toEqual('WIP');
+
+    expect(
+      parseCommitMessage(`(#123, #45) WIP: ${COMMIT_MESSAGE}`).type
+    ).toEqual('WIP');
+
+    expect(
+      parseCommitMessage(`(#123,#45) WIP: ${COMMIT_MESSAGE}`).type
+    ).toEqual('WIP');
+  });
+
+  it('should return correct WIP status', () => {
+    expect(
+      parseCommitMessage(`(#1) ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#1, #2) ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#1,#2) ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#123) chore: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#123, #45) chore: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#123,#45) chore: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#123) WIP: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(true);
+
+    expect(
+      parseCommitMessage(`(#123, #45) WIP: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(true);
+
+    expect(
+      parseCommitMessage(`(#123,#45) WIP: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(true);
+
+    // WIP must be all uppercase to match
+    expect(
+      parseCommitMessage(`(#123) wip: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#123) Wip: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+
+    expect(
+      parseCommitMessage(`(#123) WiP: ${COMMIT_MESSAGE}`).isWip
+    ).toEqual(false);
+  });
+
+  it('should return correct subject', () => {
+    expect(
+      parseCommitMessage(`(#1) ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#1234) ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#1, #2) ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#1,#2) ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#1, #34) ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#1,#34) ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#123) chore: ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#123, #45) chore: ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#123,#45) chore: ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#123) WIP: ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#123, #45) WIP: ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+
+    expect(
+      parseCommitMessage(`(#123,#45) WIP: ${COMMIT_MESSAGE}`).subject
+    ).toEqual(COMMIT_MESSAGE);
+  });
+});
