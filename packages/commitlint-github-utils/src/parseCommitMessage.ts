554bae0fd7e37ad486de080caca85e5d4c16febc
Dan Rivett <dan.rivett@elevai.com>
2020-02-15 15:58:54 -0800

    Implement working parseCommitMessage() method.

--- packages/commitlint-github-utils/src/parseCommitMessage.ts
+++ packages/commitlint-github-utils/src/parseCommitMessage.ts
@@ -1,25 +1,48 @@
-import { CommitParser, ParserOptions, ParsedCommitMessage } from '../@types';
+import { CommitParser, ParsedCommitMessage } from '../@types';
 import {
+  ISSUE_NUMBER_PATTERN,
   ISSUE_NUMBERS_PATTERN,
   WIP_WITHOUT_ISSUE_NUMBER_PATTERN,
+  ISSUE_NUMBERS_SEPARATOR,
   WIP_TYPE,
   COMMIT_DESCRIPTION_SEPARATOR,
 } from './commitlintGitHubConstants';
 
-const parseCommit = (rawCommitMessage: string, regex: RegExp): { [key: string]: string } | undefined => {
-  return regex.exec(rawCommitMessage)?.groups;
+const parseRegex = (stringToParse: string, regex: RegExp): { [key: string]: string } | undefined => {
+  return regex.exec(stringToParse)?.groups;
+};
+
+const parseIssue = (issueString: string): number => {
+  const groupsMatched = ISSUE_NUMBER_PATTERN.exec(issueString.trim())?.groups;
+
+  if (groupsMatched) {
+    return parseInt(groupsMatched.issueNumber, 10);
+  }
+
+  return -1;
 };
 
 const parseIssues = (issuesString: string): number[] => {
-  // TODO: Implement
-  return [0];
+  if (!issuesString) {
+    return [];
+  }
+
+  const issueStrings = issuesString.trim().split(ISSUE_NUMBERS_SEPARATOR);
+  const parsedIssues = issueStrings.map(str => parseIssue(str)).filter(issueNumber => issueNumber !== -1); // filter unparsed
+
+  // Only return the parsed result is ALL issue strings are valid
+  if (parsedIssues.length === issueStrings.length) {
+    return parsedIssues;
+  }
+
+  // Otherwise return an empty array
+  return [];
 };
 
 const parseDescription = (groups: { [key: string]: string }): [string, string[]] => {
   const descriptionLines = groups.description.split(COMMIT_DESCRIPTION_SEPARATOR);
 
-  const [description] = descriptionLines;
-  const [subject] = description;
+  const subject = descriptionLines[0].trim();
   const body = descriptionLines.slice(1);
 
   return [subject, body];
@@ -33,9 +56,11 @@ const parseCommitMessage: CommitParser = (rawCommitMessage: string): ParsedCommi
   let subject: string | undefined;
   let body: string[] = [];
 
-  const issueNumbersWithPossibleTypeGroups = parseCommit(rawCommitMessage, ISSUE_NUMBERS_PATTERN);
+  const issueNumbersWithPossibleTypeGroups = parseRegex(rawCommitMessage, ISSUE_NUMBERS_PATTERN);
+
   if (issueNumbersWithPossibleTypeGroups) {
-    rawIssueNumbers = issueNumbersWithPossibleTypeGroups.issue;
+    // console.log(`description: '${issueNumbersWithPossibleTypeGroups.description}'; raw: ${rawCommitMessage}`);
+    rawIssueNumbers = issueNumbersWithPossibleTypeGroups.issues.trim();
     issueNumbers = parseIssues(rawIssueNumbers);
 
     // eslint-disable-next-line prefer-destructuring
@@ -44,7 +69,7 @@ const parseCommitMessage: CommitParser = (rawCommitMessage: string): ParsedCommi
 
     [subject, body] = parseDescription(issueNumbersWithPossibleTypeGroups);
   } else {
-    const wipCommitGroups = parseCommit(rawCommitMessage, WIP_WITHOUT_ISSUE_NUMBER_PATTERN);
+    const wipCommitGroups = parseRegex(rawCommitMessage, WIP_WITHOUT_ISSUE_NUMBER_PATTERN);
     if (wipCommitGroups) {
       isWip = true;
       type = WIP_TYPE;
