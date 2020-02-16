91a4319d8d3ba213eb5aee007e1dee96e42b434d
Dan Rivett <dan.rivett@elevai.com>
2020-02-15 16:57:04 -0800

    (#1) Adding new rule: githubIssueNumberFormatRuleResolver.

--- packages/commitlint-plugin-github-rules/src/rules/githubIssueNumberFormatRuleResolver.ts
+++ packages/commitlint-plugin-github-rules/src/rules/githubIssueNumberFormatRuleResolver.ts
@@ -7,10 +7,10 @@ const githubIssueNumberFormatRuleResolver: RuleResolver<void> = parsed => {
 
   const commitMessage = parseCommitMessage(rawCommitMessage);
 
-  const issueNumbersInvalid = commitMessage.rawIssueNumbers != null && commitMessage.issueNumbers.length === 0;
+  const issueNumbersValid = commitMessage.rawIssueNumbers == null || commitMessage.issueNumbers.length > 0;
 
   return [
-    issueNumbersInvalid,
+    issueNumbersValid,
     'the commit message has an invalid issue number prefix. It must start with the issue number (or numbers if comma-separated) wrapped in parentheses. For example: (#42) My git commit message.',
   ];
 };
