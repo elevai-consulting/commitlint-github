f2fa09c4ae1e16f6cf05444835f302bac1b53c10
Dan Rivett <dan.rivett@elevai.com>
2020-02-21 11:43:52 -0800

    Adding in type definitions for rule resolvers.

--- packages/commitlint-plugin-github-rules/@types/index.d.ts
+++ packages/commitlint-plugin-github-rules/@types/index.d.ts
@@ -1,23 +1,23 @@
+import { When } from 'commitlint-github-utils/@types';
+
 export type RuleResolverResult = [boolean, string?];
 
-export type RuleResolver<T> = (
-  parsed: Partial<{
-    type: string;
-    scope: string;
-    subject: string;
-    merge: string;
-    header: string;
-    body: string;
-    footer: string;
-    notes: string[];
-    references: string[];
-    mentions: string[];
-    revert: string;
-    raw: string;
-  }>,
-  when?: string,
-  value?: T,
-) => RuleResolverResult;
+export type BaseParsedCommit = Partial<{
+  type: string;
+  scope: string;
+  subject: string;
+  merge: string;
+  header: string;
+  body: string;
+  footer: string;
+  notes: string[];
+  references: string[];
+  mentions: string[];
+  revert: string;
+  raw: string;
+}>;
+
+export type RuleResolver<T> = (parsed: BaseParsedCommit, when?: When, value?: T) => RuleResolverResult;
 
 export interface CommitlintPluginGitHub {
   rules: {
