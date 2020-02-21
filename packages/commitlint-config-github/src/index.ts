3f57914bbef3d3db79307bb9481c3da549ab2094
Dan Rivett <dan.rivett@elevai.com>
2020-02-21 12:01:02 -0800

    Updating the default rule configuration to use the new rules added.

--- packages/commitlint-config-github/src/index.ts
+++ packages/commitlint-config-github/src/index.ts
@@ -1,10 +1,40 @@
-import { commitlintGitHubConstants } from 'commitlint-github-utils';
+import { When } from 'commitlint-github-utils/@types';
+import utils from 'commitlint-github-utils';
 import { CommitlintConfigGitHub } from '../@types';
+
+const commitlintGitHubRules = utils.commitlintGitHubConstants.GITHUB_RULES;
+
+const supportedTypes = [
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'feature', // Additional one for those who prefer a full word
+  'fix',
+  'improvement',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test',
+];
 
 export const commitlintConfigGitHub: CommitlintConfigGitHub = {
   rules: {
-    [commitlintGitHubConstants.GITHUB_RULES.issueNumberMissing]: [2, 'always', true],
-    [commitlintGitHubConstants.GITHUB_RULES.issueNumberFormat]: [2, 'always'],
+    [commitlintGitHubRules.issueNumberMissing]: [2, When.NEVER],
+    [commitlintGitHubRules.issueNumberFormat]: [2, When.ALWAYS],
+    [commitlintGitHubRules.issueNumberDuplicate]: [2, When.NEVER],
+
+    [commitlintGitHubRules.wipAllowed]: [2, When.ALWAYS],
+
+    [commitlintGitHubRules.subjectEmpty]: [2, When.NEVER],
+    [commitlintGitHubRules.subjectSeparator]: [2, When.ALWAYS],
+    [commitlintGitHubRules.subjectCase]: [2, When.ALWAYS, 'sentence-case'],
+    [commitlintGitHubRules.subjectFullStop]: [1, When.ALWAYS], // Warning only
+
+    [commitlintGitHubRules.typeCase]: [2, When.ALWAYS, 'lower-case'],
+    [commitlintGitHubRules.typeEnum]: [2, When.ALWAYS, supportedTypes],
   },
 };
 
