e868378b6605ec4bca1f8d7b10fa3e25060ebb11
Dan Rivett <dan.rivett@elevai.com>
2020-02-21 12:00:36 -0800

    (#1) Increasing required code coverage levels to maintain current coverage levels.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  collectCoverageFrom: [
-    'packages/**/*.(ts)',
-    '!packages/commitlint-github-e2e-tests/**',
-  ],
+  collectCoverageFrom: ['packages/**/*.(ts)', '!packages/commitlint-github-e2e-tests/**'],
   coverageThreshold: {
     global: {
-      statements: 86,
-      branches: 68,
-      functions: 94,
-      lines: 93,
+      statements: 90,
+      branches: 90,
+      functions: 95,
+      lines: 95,
     },
   },
   testRegex: 'tests/.*\\.test\\.ts$',
   testPathIgnorePatterns: ['/packages/commitlint-github-e2e-tests/'],
   preset: 'ts-jest',
-}
+};
