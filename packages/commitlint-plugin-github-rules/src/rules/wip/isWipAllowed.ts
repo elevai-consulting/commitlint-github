6a50e80ef803bfc953507387622677e286b7e68a
Dan Rivett <dan.rivett@elevai.com>
2020-02-21 11:42:14 -0800

    Adding in isWip rule to allow or prevent WIP commits. For example PR validation could prevent WIP commits from being merged.

---
+++ packages/commitlint-plugin-github-rules/src/rules/wip/isWipAllowed.ts
@@ -0,0 +1,15 @@
+import utils from 'commitlint-github-utils';
+import { RuleResolver } from '../../../@types';
+
+const wipAllowedRuleResolver: RuleResolver<void> = (parsed, when?: string) => {
+  const rawCommitMessage = parsed.raw;
+  if (!rawCommitMessage) return [false, 'Commit message should not be empty'];
+
+  const commitMessage = utils.parseCommitMessage(rawCommitMessage);
+
+  const result = !commitMessage.isWip || !utils.isNegated(when);
+
+  return [result, 'WIP commits are not permitted'];
+};
+
+export default wipAllowedRuleResolver;
---
+++ packages/commitlint-plugin-github-rules/src/rules/wip/tests/isWipAllowed.test.ts
@@ -0,0 +1,49 @@
+import { When } from 'commitlint-github-utils/@types';
+
+import { RuleResolverResult } from '../../../../@types';
+import wipAllowedRuleResolver from '../isWipAllowed';
+import runRule from '../../utils/tests/utils';
+
+const parse = (when: When, rawCommitMessage: string): RuleResolverResult =>
+  runRule(wipAllowedRuleResolver, [when], rawCommitMessage);
+
+describe('wipAllowedRuleResolver', () => {
+  it('should always return an error response if an empty commit message is provided', () => {
+    expect(parse(When.ALWAYS, '')[0]).toEqual(false);
+    expect(parse(When.NEVER, '')[0]).toEqual(false);
+  });
+
+  it('should return a success response if the commit is not a WIP', () => {
+    expect(parse(When.ALWAYS, '(#1) my commit message')[0]).toEqual(true);
+    expect(parse(When.ALWAYS, '(#1) chore: my commit message')[0]).toEqual(true);
+
+    expect(parse(When.ALWAYS, '(#1, #2) my commit message')[0]).toEqual(true);
+    expect(parse(When.ALWAYS, '(#1,#2) my commit message')[0]).toEqual(true);
+
+    expect(parse(When.ALWAYS, '(#123, #23) chore: my commit message')[0]).toEqual(true);
+    expect(parse(When.ALWAYS, '(#123,#23) chore: my commit message')[0]).toEqual(true);
+  });
+
+  it('should return a success response if the commit is WIP and not disallowed', () => {
+    expect(parse(When.ALWAYS, '(#1) WIP: my commit message')[0]).toEqual(true);
+    expect(parse(When.ALWAYS, '(#1, #2, #3, #4) WIP: my commit message')[0]).toEqual(true);
+    expect(parse(When.ALWAYS, '(#1,#2,#3,#4) WIP: my commit message')[0]).toEqual(true);
+  });
+
+  it('should return a success response if the commit is not a WIP even if disallowed', () => {
+    expect(parse(When.NEVER, '(#1) my commit message')[0]).toEqual(true);
+    expect(parse(When.NEVER, '(#1) chore: my commit message')[0]).toEqual(true);
+
+    expect(parse(When.NEVER, '(#1, #2) my commit message')[0]).toEqual(true);
+    expect(parse(When.NEVER, '(#1,#2) my commit message')[0]).toEqual(true);
+
+    expect(parse(When.NEVER, '(#123, #23) chore: my commit message')[0]).toEqual(true);
+    expect(parse(When.NEVER, '(#123,#23) chore: my commit message')[0]).toEqual(true);
+  });
+
+  it('should return an error response if the commit is WIP and disallowed', () => {
+    expect(parse(When.NEVER, '(#1) WIP: my commit message')[0]).toEqual(false);
+    expect(parse(When.NEVER, '(#1, #2, #3, #4) WIP: my commit message')[0]).toEqual(false);
+    expect(parse(When.NEVER, '(#1,#2,#3,#4) WIP: my commit message')[0]).toEqual(false);
+  });
+});
